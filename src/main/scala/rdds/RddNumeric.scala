c5c6fb58e45624b90348d135c63d1bcc141d356f
Jimmy Guarin <jimmyguarin43@gmail.com>
2024-07-16 08:10:28 -0500

    RDDS: Add a numeric example

--- src/main/scala/rdds/RddNumeric.scala
+++ src/main/scala/rdds/RddNumeric.scala
@@ -1,9 +1,28 @@
 package jimmyguarin
 package rdds
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 object RddNumeric {
 
+  // the elements are number
+  // you have extra methods like : sum, max, min, stats, mean, variance
 
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Demo").setMaster("local[1]")
+    val sc = SparkContext.getOrCreate(conf)
 
+    val nrdd = sc.parallelize(Range(1, 1000)).cache()
 
+    // stats summary
+    val sts = nrdd.stats()
+
+    println("Stats Summary", sts)
+
+    // get  outliers
+    val stddev = sts.stdev
+    val avg = sts.mean
+    val frdd = nrdd.filter(n => Math.abs(n - avg) < (3 * stddev)).cache() // use cache because we are gonna use it more than once
+    println("NUmber of outliers", sts.count - frdd.count)
+  }
 }
